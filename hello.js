fc057a66598d42bfd79457069a51061304b41d91
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2019-09-21 13:09:26 -0400

    Reworking for ilc

--- hello.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  debugger;
-  console.log({request});
-  if (request.text) {
-    document.querySelector('#text').innerHTML = request.text;
-  }
-  sendResponse({farewell: "goodbye"});
-  return true;
-});
